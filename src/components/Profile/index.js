d7ed1ca4e6f248562e5c1699f94f49761ec86e4b
Bruno Lima <networkbruno@gmail.com>
2020-02-12 16:35:02 -0300

    Imports Layout

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Bruno Lima</h1>
+        <h2>Programador Backend</h2>
+        <p>Membro da empresa full de tecnologia da informação.</p>
+    </div>
+)
+
+export default Profile
