02d12850b49e4bdd55a66733d8741308e56ba898
Bruno Lima <networkbruno@gmail.com>
2020-02-20 00:51:16 -0300

    Integrate layout to Blog Post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,20 +1,23 @@
 import React from "react"
 import { graphql } from "gatsby"
-/*2º - metodo BlogPost recebendo o valor 
+/*2º - metodo BlogPost recebendo o valor */
 
-*/
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
 const BlogPost = ({ data }) => {
     const post = data.markdownRemark
 
     //Renderizando o post
     return (
-        <>
+        <Layout>
+            <SEO title={post.frontmatter.title} />
             <h1>{post.frontmatter.title}</h1>
             {/*a div está recebendo o html do post.html, por ser dados HTML o Reacat informa
             que é arriscado passar html e tem que ter o total conhecimento do html, pois podem acontecer ataques de xss. O nome "dangerousylSetInnerHTML",
             substitui o "innerHTML". Então precisa estar garantido a etrutura do html antes de passar.  */}
             <div dangerouslySetInnerHTML={{ __html: post.html}}></div>
-        </>
+        </Layout>
     )
 }
 //1º - Executando a query e exportando 
