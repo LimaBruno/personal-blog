8dcf76824098fed95f3a8589241f8dc1b353a0ea
Bruno Lima <networkbruno@gmail.com>
2020-03-03 19:53:11 -0300

    TransitionPortal_Configuration

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,11 +7,12 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import { TransitionPortal } from "gatsby-plugin-transition-link"; //Para controlar o comportamento dos efeitos de link
+//TransitionPortal = Existe 3 tipos de level (top, middle, bottom)
+
 import Sidebar from "../Sidebar"
 import MenuBar from "../MenuBar"
-
 //import styled from "styled-components" Importando o Stlyed-components - Não precisa mais, porque está chamadando do "layout/styled.js"
-
 import * as S from "./styled" // Organizando, jogando todos os componentes de estilo do layout para o arquivo styled.js e colocando uma alias. "S"
 import GlobalStyle from "../../styles/global" //importando o rest global
 
@@ -20,11 +21,13 @@ const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper> {/* Para organizar css colocou no styled.css e exportou "export" a const  */}
       <GlobalStyle /> {/* importando o rest global */}
-      <aside>
+      <TransitionPortal level="top"> {/* o top fica por cima do efeito  */}
         <Sidebar/>
-      </aside>
+      </TransitionPortal>      
       <S.LayoutMain>{children}</S.LayoutMain>
-      <MenuBar />
+      <TransitionPortal level="top">
+        <MenuBar />
+      </TransitionPortal>  
     </S.LayoutWrapper>
   )
 }
