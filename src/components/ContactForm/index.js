184887cf7ae557f0ff9dc90bb877f0a721049f19
Bruno <networkbruno@gmail.com>
2020-08-01 01:39:00 -0300

    New Update ContactForm

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -22,27 +22,27 @@ import {
 
 const RECAPTCHA_KEY = process.env.GATSBY_SITE_RECAPTCHA_KEY
 const encode = (data) => {
-	return Object.keys(data).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
-};
+	return Object.keys(data).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&')
+}
 const ContactForm = () => {
-	const { register, handleSubmit, errors, setValue } = useForm();
-	const [ feedbackMsg, setFeedbackMsg ] = useState(null);
-	const [ captchaMsg, setCaptchaMsg ] = useState(null);
-	let captchaRef = useRef(null);
+	const { register, handleSubmit, errors, setValue } = useForm()
+	const [ feedbackMsg, setFeedbackMsg ] = useState(null)
+	const [ captchaMsg, setCaptchaMsg ] = useState(null)
+	let captchaRef = useRef(null)
 
 	useEffect(
 		() => {
-			register({ required: 'Required', name: 'g-recaptcha-response' });
+			register({ required: 'Required', name: 'g-recaptcha-response' })
 		},
 		[ register ]
-	);
+	)
 	const onSubmit = (data, e) => {
-		e.preventDefault();
-		const captchaValue = captchaRef.current.getValue();
+		e.preventDefault()
+		const captchaValue = captchaRef.current.getValue()
 		
 		if (!captchaValue) {
-			console.log('CAPTCHA missing!');
-			setCaptchaMsg('Selecione "Não sou um robô"');
+			console.log('CAPTCHA Missing!');
+			setCaptchaMsg('Selecione "Não sou um robô"')
 			return;
 		}
 		fetch('/', {
@@ -57,13 +57,13 @@ const ContactForm = () => {
 			.then((response) => {
 				// console.log(response);
 				e.target.reset();
-				setFeedbackMsg(`😀 Sua mensagem foi enviada com sucesso!`);
+				setFeedbackMsg(`😀 Sua mensagem foi enviada com sucesso!`)
 			})
 			.catch((error) => {
-				setFeedbackMsg('😥 Oops, não foi possível enviar sua mensagem, tente novamente!');
-				console.log(error);
-			});
-	};
+				setFeedbackMsg('😥 Oops, Erro! Por favor, tente novamente mais tarde.')
+				console.log(error)
+			})
+	}
 	return (
     <ContactWrapper>
         <ContactTitle>Vamos Conversar?</ContactTitle>
@@ -76,85 +76,83 @@ const ContactForm = () => {
 			onSubmit={handleSubmit(onSubmit)}
 			name="contact"
 			method="post"
-			netlify
+			data-netlify-recaptcha="true"
 			netlify-honeypot="bot-field"
 			data-netlify="true"
-			data-netlify-honeypot="bot-field"
-			data-netlify-recaptcha="true"
 		>
-			<ContactInput type="hidden" name="bot-field" />
-			<ContactInput type="hidden" name="form-name" value="contact" />
+			<ContactInput type="hidden" name="bot-field"/>
+			<ContactInput type="hidden" name="form-name" value="contact"/>
 			<LabelMain for="name"><LabelName>Nome:</LabelName>
 				<ContactInput
-					type="text"
-					id="name"
-					name="name"
-					placeholder="Seu nome"
-					aria-label="name"
-					ref={register({
-            required: "Campo obrigatório",
-            maxLength: {
-                value: 30,
-                message: "No máximo 30 caracteres"
-            }
-          })}
+				  type="text"
+				  id="name"
+				  name="name"
+				  placeholder="Seu nome"
+				  aria-label="name"
+				  ref={register({
+					required: "Campo obrigatório",
+					maxLength: {
+					  value: 30,
+					  message: "No máximo 30 caracteres"
+					}
+				  })}
 				/>
-        {errors.name && <Error>{errors.name.message}</Error>}
-			</LabelMain>			
+        		{errors.name && <Error>{errors.name.message}</Error>}
+			</LabelMain>
 			<LabelMain for="email"><LabelName>Email:</LabelName>
 				<ContactInput
-					type="text"
-					id="email"
-					placeholder="Seu email" 
-					name="email"
-					aria-label="email"
-					ref={register({
-            required: "Campo obrigatório",
-            pattern: {
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-              message: "E-mail inválido"
-             }
-           })}
+				  type="text"
+				  id="email"
+				  placeholder="Seu email" 
+				  name="email"
+				  aria-label="email"
+				  ref={register({
+					required: "Campo obrigatório",
+					pattern: {
+					  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+					  message: "E-mail inválido"
+					}
+				  })}
 				/>
-        {errors.email && <Error>{errors.email.message}</Error>}
-			</LabelMain>			
+        		{errors.email && <Error>{errors.email.message}</Error>}
+			</LabelMain>
 			<LabelMain for="message"><LabelName>Mensagem:</LabelName>
 				<ContactArea
-					name="message"
-          id="message"
-          rows="5"
-          cols="33"
-          alt="Campo Mensagem"
-          aria-label="message"
-          placeholder="Sua mensagem ..."
-          ref={register({
-            required: "Campo obrigatório",
-            maxLength: {
-                value: 180,
-                message: "No máximo 180 caracteres"
-            }
-          })}
-        />
-        {errors.message && <Error>{errors.message.message}</Error>}
+				  name="message"
+          		  id="message"
+          		  rows="5"
+          		  cols="33"
+          		  alt="Campo Mensagem"
+          		  aria-label="message"
+          		  placeholder="Sua mensagem ..."
+          		  ref={register({
+            		required: "Campo obrigatório",
+            		maxLength: {
+                	  value: 180,
+                	  message: "No máximo 180 caracteres"
+            		}
+          		  })}
+        		/>
+        		{errors.message && <Error>{errors.message.message}</Error>}
 			</LabelMain>
 			<Recaptcha>
-        <ReCAPTCHA
-          name="g-recaptcha-response"
-          ref={captchaRef}
-          sitekey={RECAPTCHA_KEY}
-          onChange={(val) => {
-            setValue('g-recaptcha-response', val, true);            
-          }}
-        />
-      </Recaptcha>
+				<ReCAPTCHA
+				  name="g-recaptcha-response"
+				  ref={captchaRef}
+				  sitekey={RECAPTCHA_KEY}
+				  onChange={(val) => {
+					setValue('g-recaptcha-response', val, true)
+				  }}
+				/>
+      		</Recaptcha>
 			{captchaMsg && <Error>{captchaMsg}</Error>}
-      <br/>
-      {feedbackMsg && <h2>{feedbackMsg}</h2>}
-      <br/>
+      		<br/>
+      		{feedbackMsg && <h3>{feedbackMsg}</h3>}
+      		<br/>
 			<ContactButtonSend type="submit" aria-label="Enviar Formulário Email"><IconSend/>Enviar</ContactButtonSend>
-      <ContactButtonRest type="reset" aria-label="Apagar Formulário Email"><IconRest/>Apagar</ContactButtonRest>                    
+      		<ContactButtonRest type="reset" aria-label="Apagar Formulário Email"><IconRest/>Apagar</ContactButtonRest>
 		</ContactFormMain>
-    <MarginDiv/>
+    	<MarginDiv/>
     </ContactWrapper>
 	)
 }
