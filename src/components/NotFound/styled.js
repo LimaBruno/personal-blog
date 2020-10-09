436308978f097b2d62c0336ac3f6a644febfe738
Bruno <networkbruno@gmail.com>
2020-10-09 19:21:52 -0300

    Update NotFound Component

--- src/components/NotFound/styled.js
+++ src/components/NotFound/styled.js
@@ -1,13 +1,9 @@
 import styled from "styled-components"
-import media from "styled-media-query"
 
-export const BackgroundImage = styled.section`
-    background-image: url("/assets/img/deckfail.png");
-    background-repeat: no-repeat;
-    background-size: 280px;
-    height: 100vh;
-    width: 100vw;
-    ${media.lessThan("large")`
-        background-position-x: center;
-   `}
+export const BackgroundImage = styled.section`    
+  background-image: url("/assets/img/brunocesarlima_deckfail.png");
+  background-repeat: no-repeat;
+  height: 50vh;
+  width: 100wh;
+  margin: 1.5rem;
 `
